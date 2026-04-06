06b3e31c00df27655319ef218a718e8caad60704
Giovanna Natale <giovannanicole2804@gmail.com>
2026-04-06 19:47:25 -0300

    Atualiza projeto após aula 06/04

---
+++ scripts/00- observacoes.R
@@ -0,0 +1,67 @@
+# https://washingtonsilva.github.io/site_bach_icd2026
+# *LINK DE INTRODUÇÃO A CIENCIA DE DADOS*
+
+
+
+
+#Variáveis são características podem ser medidas ou observadas.(colunas)
+
+#Observações são as unidades (ou casos) sobre os quais as variáveis são observadas ou medidas. (linhas)
+
+# Ctrl + S = Salvar
+# Ctrl + L = limpa o console
+# Ctrl + shift + R = Criar uma sessão
+# Ctrl + shift + M = |> 
+# Alt + - = <-  (atribuição)
+
+# Pipeline = sequencia de operações
+
+# Data frames e tibbles são estruturas de dados formadas por linhas(observações) e
+# colunas (variáveis), sendo que cada coluna (variável) pode assumir um tipo diferente. 
+# É a estrutura mais comum para armazenar dados para análises estatísticas.
+
+# A função filter() permite escolher quais observações (linhas) você quer manter ou remover de uma data frame/tibble, com base em condições lógicas.
+
+# Sintaxe básica
+
+# com o operador pipe
+df |> filter(condição)
+
+# df: A data frame/tibble a ser filtrada
+# condição: Expressão lógica que retorna TRUE/FALSE para cada linha
+# Somente as linhas que resultam em TRUE são mantidas no resultado
+
+# Tipo	Operador	Descrição
+# Comparação	==	igual
+# !=	diferente
+#>	maior que
+#<	menor que
+#>=	maior ou igual
+#<=	menor ou igual
+#Lógicos	&	E (todas as condições verdadeiras) (juntar condições)
+# |	OU (pelo menos uma verdadeira)
+#!	NÃO (negação)
+#Pertinência	%in%	pertence a um conjunto
+
+#Você pode se deparar com códigos que usam , (vírgula) ao invés de & para representar o “E” lógico em filter()
+#Exemplo: filter(cidade == "Formiga", receita > 1000).
+#Nesta disciplina usaremos somente o operador & para representar o “E” lógico, pois considero que o código fica mais claro e legível.
+#Operadores usados com filter()
+#Exemplos
+#Operadores usados com filter()
+#Nota sobre os operadores &, | e %in%
+#  Operador	Uso principal	Exemplo
+#&	exige que todas as condições sejam verdadeiras	cidade == "Formiga" & receita > 1000
+# |	exige que pelo menos uma condição seja verdadeira	cidade == "Formiga" | cidade == "Arcos"
+#%in%	verifica se um valor pertence a um conjunto	cidade %in% c("Formiga", "Arcos")
+#Ideia central
+#Use & para impor condições simultâneas.
+#Use | para combinar alternativas.
+#Use %in% para escrever de forma mais compacta várias comparações com | para a mesma variável.
+#A função select()
+#Função select()
+#Seleciona as colunas/variáveis desejadas
+#A função mutate()
+
+# Cada tipo de arquivo tem um tipo de função para ler 
+
--- scripts/02-importacao-manipulacao.R
+++ scripts/02-importacao-manipulacao.R
@@ -5,20 +5,32 @@
 # 1. Importar um arquivo csv de dados
 # 2. Preparar os dados para análise
 # 3. Aprender as funções básicas de manipulação de dados do pacote dplyr
+#===============================================================================
 
+
+
+#===============================================================================
 
 # Configurações globais ---------------------------------------------------
 
+#===============================================================================
+
 # Configura o número de dígitos a serem exibidos
-options(digits = 5, scipen = 999)
+options(digits = 5, scipen = 999) #para não exibir números em notação cientifica
 
 # carrega os pacotes necessários
 library(here) # para usar caminhos relativos
 library(tidyverse) # carrega o dplyr, readr, ggplot2, etc.
 library(janitor) # para limpar os nomes das colunas
+#===============================================================================
 
+
+
+#===============================================================================
 
 # Aquisição dos dados ----------------------------------------------------
+
+#===============================================================================
 
 # define o caminho relativo do arquivo de dados
 caminho_csv <- here("dados/brutos/dados_vendas.csv")
@@ -40,9 +52,15 @@ tail(dados_vendas)
 
 # resumo estatístico das colunas
 summary(dados_vendas)
+#===============================================================================
 
+
+
+#===============================================================================
 
 # Preparação dos dados ----------------------------------------------------
+
+#===============================================================================
 
 # pipeline de preparação dos dados
 dados_vendas_limpos <- dados_vendas |>
@@ -59,3 +77,254 @@ dados_vendas_limpos <- dados_vendas |>
 
 # verifica a estrutura dos dados
 glimpse(dados_vendas_limpos)
+
+#Variáveis são características podem ser medidas ou observadas.(colunas)
+
+#Observações são as unidades (ou casos) sobre os quais as variáveis são observadas ou medidas. (linhas)
+
+
+# Salva os dados limpos em um arquivo rds para aálises futuras sem precisar repetir 
+# a preparação dos dados
+
+## 1. Define o caminho relativo para salvar o arquivo rds
+caminho_rds <- here ("dados/limpos/dados_vendas_limpos.rds")
+
+## 2. Salva o objeto dados_vendas_limpos no formato rds
+readr::write_rds(dados_vendas_limpos,caminho_rds)
+
+# Lendo os dados limpos em uma seção futura
+
+## 1. Defina o caminho relativo do arquivo rds
+caminho_rds <- here("dados/limpos/dados_vendas_limpos.rds")
+
+## 2. Lê o arquivo rds e armazena em um objeto
+dados_vendas_limpos <- readr::read_rds (caminho_rds)
+#===============================================================================
+
+
+
+#===============================================================================
+
+# A FUNÇÃO FILTER ---------------------------------------------------------
+
+#===============================================================================
+
+# Filtra as vendas realizadas na cidade de "Formiga"
+dados_vendas_limpos |> 
+  filter(cidade == "Formiga")
+
+# Filtra as vendas realizadas por um representante específico
+dados_vendas_limpos |> 
+  filter(representante == "Representante 1")
+
+# Filtra as vendas realizadas em Formiga por um representante específico
+dados_vendas_limpos |> 
+  filter(cidade == "Formiga" & representante == "Representante 1")
+
+# Filtra as vendas realizadas em Formiga ou em Arcos com o operador |
+dados_vendas_limpos |> 
+  filter(cidade == "Formiga" | cidade == "Arcos")
+
+# Filtra as mesmas vendas usando %in%, uma forma mais compacta para 
+# múltiplas comparações da mesma variável
+dados_vendas_limpos |> 
+  filter(cidade %in% c("Formiga", "Arcos"))
+
+# Salva o resultado em um novo objeto 
+dados_vendas_formiga_arcos <- dados_vendas_limpos |> 
+  filter(cidade %in% c("Formiga", "Arcos"))
+
+# Exibe o resultado
+dados_vendas_formiga_arcos
+#===============================================================================
+
+
+
+#===============================================================================
+
+# A FUNÇÃO SELECT ---------------------------------------------------------
+
+#===============================================================================
+
+# seleciona apenas as colunas cidade, produto e receita
+dados_vendas_limpos |> 
+  select(cidade, produto, receita)
+
+# remove as coluna representante e cidade 
+dados_vendas_limpos |> 
+  select(-cidade, - representante)
+
+# salvando o resultado em um novo objeto
+dados_vendas_selecionados <- dados_vendas_limpos |> 
+  select(cidade, produto, receita)
+
+# exibe o resultado
+dados_vendas_selecionados
+
+#===============================================================================
+
+
+
+#===============================================================================
+
+# A FUNÇÃO MUTATE ---------------------------------------------------------
+
+#===============================================================================
+
+
+# Cria a variável prec0_desconto (10% sobre o preco_unitario)
+dados_vendas_limpos |> 
+  mutate(preco_desconto = preco_unitario * 0.9)
+
+# Cria a variável receita_total 
+dados_vendas_limpos |> 
+  mutate(receita_total = unidades * preco_unitario)
+
+# Cria a variável receita total, agrupa em cidade, calcula a receita total
+# por cidade e ordena o resultado
+dados_vendas_limpos |> 
+  mutate(receita_total = unidades * preco_unitario) |> 
+  group_by(cidade) |> 
+  summarise(receita_tota_cidade = sum(receita_total)) |> 
+  arrange(desc(receita_tota_cidade))
+
+# Cria a variável categoria_receita
+dados_vendas_limpos |> 
+  mutate(categoria_receita = ifelse(receita > 1000, "Alta", "Baixa")) |> 
+  select(cidade, produto, categoria_receita)
+
+# Cria a variável categoria_receita com multiplas categorias
+dados_vendas_limpos |> 
+  mutate(categoria_receita = case_when(
+    receita > 1000 ~ "Alta",
+    receita > 500 & receita <= 1000 ~ "Média",
+    receita > 0 & receita <= 500 ~ "Baixa",
+    TRUE ~ "Sem Receita"
+  )) |> 
+  select(cidade, produto, categoria_receita)
+#===============================================================================
+
+
+
+#===============================================================================
+
+# AS FUNÇÕES SUMMARISE E GROUP_BY  ----------------------------------------
+
+#===============================================================================
+
+# Calcula a receita média
+dados_vendas_limpos |> 
+  summarise(receita_media = mean(receita))
+
+# Calcula a receita total
+dados_vendas_limpos |> 
+  summarise(receita_total = sum(receita))
+
+# Calcula o número de representantes distintos nos dados
+dados_vendas_limpos |> 
+  summarise(numero_representantes = n_distinct(representante))
+
+# Calcula o número total de vendas realizadas
+dados_vendas_limpos |> 
+  summarise(total_vendas = n())
+
+# Calcula a receita média por cidade 
+dados_vendas_limpos |> 
+  group_by(cidade) |> 
+  summarise(receita_media = mean(receita))
+
+# Calcula a receita média por produto
+
+dados_vendas_limpos |> 
+  group_by(produto) |> 
+  summarise(receita_media = mean(receita))
+
+# Calcula a receita média por cidade produto
+dados_vendas_limpos |> 
+  group_by(cidade, produto) |> 
+  summarise(receita_media = mean(receita))
+#===============================================================================
+
+
+
+#===============================================================================
+
+# A FUNÇÃO ARRANGE --------------------------------------------------------
+
+#===============================================================================
+
+# Ordena os dados por receita em ordem crescente
+dados_vendas_limpos |> 
+  arrange(receita)
+
+# Ordena os dados por receita em ordem decrescente 
+dados_vendas_limpos |> 
+  arrange(desc(receita))
+
+# Ordena a receita média por cidade em ordem crescente
+dados_vendas_limpos |> 
+  group_by(cidade) |> 
+  summarise(receita_media = mean(receita)) |> 
+  arrange(receita_media)
+  
+# Ordena a receita média por cidade em ordem decrescente
+# salva o resultado em um novo objeto
+receita_media_cidade <- 
+  dados_vendas_limpos |> 
+  group_by(cidade) |> 
+  summarise(receita_media = mean(receita)) |> 
+  arrange(desc(receita_media))
+
+# Exibe o resultado
+receita_media_cidade
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
